2510839d0826391bc7b5feee648ff12f6bd03c84
Whitney <whitney.desbonnes@outlook.fr>
2024-06-25 11:11:25 +0200

    ✅ F02 - App.jsx > Adding React router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import LoginPage from "./components/pages/login/LoginPage";
+import OrderPage from "./components/pages/login/OrderPage";
+import ErrorPage from "./components/pages/login/ErrorPage";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 function App() {
-  return <LoginPage/>;
+  // state (état, données)
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <LoginPage/>,
+      errorElement: <ErrorPage/>,
+    },
+    {
+      path:"/order/:inputValue",
+      element: <OrderPage/>
+    },
+  ])
+
+  // comportements
+
+  // render / affichage
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
