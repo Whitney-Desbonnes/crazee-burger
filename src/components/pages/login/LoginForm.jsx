315a16fb34ed21a8c2a8a465b162ecf15ce5dbfb
Whitney <whitney.desbonnes@outlook.fr>
2024-07-04 17:08:27 +0200

    ♻️ F03 - LOGIN > LoginForm > Refacto

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -2,10 +2,9 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import theme from "../../../theme/index";
-import logo from "../../../../public/assets/img/logo-orange.png"
-import burgerBackground from "../../../../public/assets/img/burger-background.jpg"
-import { PiUserCircleFill } from "react-icons/pi";
-import { FaAngleRight } from "react-icons/fa6";
+import { BsPersonCircle } from "react-icons/bs";
+import { IoChevronForward } from "react-icons/io5";
+import Input from "./Input";
 
 
 export default function LoginForm() {
@@ -27,37 +26,33 @@ export default function LoginForm() {
   // render / rendu
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-      <div className="logo">
-        Crazee <img src={logo} alt="Logo" /> Burger
+      <div>
+        <h1>Bienvenue chez nous&nbsp;!</h1>
+        <hr />
+        <h2>Connectez-vous</h2>
       </div>
-      <h1>Bienvenue chez nous&nbsp;!</h1>
-      <hr />
-      <h2>Connectez-vous</h2>
-      <div className="input-name">
-        <PiUserCircleFill />
-        <input type="text" onChange={handleChange} value={inputValue} placeholder="Entrez votre prénom" required/>
+      <div>
+        <Input 
+          value={inputValue} 
+          onChange={handleChange} 
+          placeholder="Entrez votre prénom" 
+          required
+          Icon={<BsPersonCircle/>}
+        />
+        <button>
+          <span>Accéder à mon espace</span>
+          <IoChevronForward />
+        </button>
       </div>
-      <button>
-        Accéder à mon espace <FaAngleRight />
-      </button>
     </LoginFormStyled>
   )
 }
 
 
 const LoginFormStyled = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: ${theme.colors.white};
-  height: 100vh;
-  background-image: url(${burgerBackground});
-  background-size: cover;
-  background-repeat: no-repeat;
-  padding: 0 20px;
+  width: 100%;
+  max-width: 400px;
 
-  .logo, 
   h1, 
   h2 {
   font-family: "Amatic SC", sans-serif;
@@ -65,29 +60,14 @@ const LoginFormStyled = styled.form`
   font-style: normal;
   text-align: center;
   }
-
-  .logo {
-    color: ${theme.colors.primary};
-    font-size: ${theme.fonts.P6};
-    display: grid;
-    grid-template-columns: 1fr auto 1fr;
-    align-items: center;
-    text-transform: uppercase;
-    margin-bottom: 40px;
-  }
-
-  .logo img {
-    width: 100%;
-    max-width: 200px;
-  }
-
+  
   h1 {
     font-size: ${theme.fonts.P5};
   }
 
 
   hr {
-    background-color: ${theme.colors.primary};
+    background-color: #F56A2C;
     height: 3px;
     width: 100%;
     max-width: 400px;
@@ -99,7 +79,6 @@ const LoginFormStyled = styled.form`
     font-size: ${theme.fonts.P4};
   }
 
-  .input-name,
   button {
     border-radius: ${theme.borderRadius.round};
     padding: 18px 24px;
@@ -107,30 +86,6 @@ const LoginFormStyled = styled.form`
     width: 100%;
     max-width: 400px;
     font-size: ${theme.fonts.P0};
-  }
-
-  .input-name {
-    background-color: ${theme.colors.white};
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 13px;
-
-    svg {
-      color:${theme.colors.greyBlue};
-      font-size: ${theme.fonts.P0};
-    }
-
-    input {
-      font-family: Arial, Helvetica, sans-serif;
-      color: #D3D3D3;
-      width: 100%;
-      border: none;
-    }
-
-    input:focus {
-      outline: none;
-    }
   }
 
   button {
