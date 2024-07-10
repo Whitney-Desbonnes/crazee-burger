7a7e85858119a3fdbe8f5d8d881d6ad64f58a7ce
Whitney <whitney.desbonnes@outlook.fr>
2024-07-10 10:52:31 +0200

    💄 F04 - Reusable ui

---
+++ src/components/pages/reusable-ui/Logo.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import theme from "../../theme/index";
+
+
+export default function Logo({onClick}) {
+
+    return(
+        <LogoStyled className="logo" onClick={onClick}>
+            <h1>Crazee</h1>
+            <img src="/images/logo-orange.png" alt="Logo" /> 
+            <h1>Burger</h1>
+        </LogoStyled>
+    )
+}
+
+const LogoStyled = styled.div`
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+    margin-bottom: 40px;
+    font-size: ${theme.fonts.P6};
+
+    h1 {
+    font-family: "Amatic SC", sans-serif;
+    font-weight: ${theme.weights.bold};
+    font-style: normal;
+    text-align: center;
+    color: ${theme.colors.primary};
+    text-transform: uppercase;
+    }
+
+    img {
+        width: 100%;
+        max-width: 200px;
+    }
+`;
